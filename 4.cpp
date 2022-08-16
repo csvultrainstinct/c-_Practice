43d0aa71fc3ab95a2acb7fdf00d9b4dde7c56989
vibhav chirravuri <44679811+csvultrainstinct@users.noreply.github.com>
2022-08-16 15:00:48 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int n;
+    cin>>n;
+    int i=2;
+    while(n--){
+        cout<<(i*i)<<" ";
+        i=i+2;
+    }
+    
+ return 0;   
+}
