1adf917088cc01f55b74f1854543b5ebe6604ee0
vibhav chirravuri <44679811+csvultrainstinct@users.noreply.github.com>
2022-08-17 10:55:31 +0530

    Update 5.cpp

--- 5.cpp
+++ 5.cpp
@@ -4,35 +4,41 @@ using namespace std;
 
 int main(){
     string name;
-    int empid,basic,sa,taxsaving;
+    double empid,basic,sa,taxsaving;
     double perc;
-    cin>>name>>empid>>basic>>sa>>taxsaving;
-    int annual=(basic+sa)*12;
-    double bonus=annual*(perc/100);
+    cin>>name>>empid>>basic>>sa>>perc>>taxsaving;
+    double annual=(basic+sa)*12;
+    double bonus=(annual*perc)/100;
     double totalAnnual=annual+bonus;
     cout<<totalAnnual<<endl;
-    int annualTaxSaving=taxsaving*12;
+    double annualTaxSaving=taxsaving*12;
     double tax;
-    if(totalAnnual<250000){
-        cout<<0<<endl;
-    }
-    else if(totalAnnual>250000 && totalAnnual<500000){
-        tax=totalAnnual*(5/100);
-        cout<<tax<<endl;
-    }
-    else if(totalAnnual>500000 && totalAnnual<1000000){
-        tax=totalAnnual*(20/100);
-        cout<<tax<<endl;
-    }
-    else{
-        tax=totalAnnual*(30/100);
-        cout<<tax<<endl;
-    }
-    if(annualTaxSaving>=150000){
+        if(totalAnnual<250000)
+        {
+          cout<<0<<endl;
+        }
+        else if(totalAnnual>250000 && totalAnnual<500000){
+           tax=5;
+           cout<<tax<<endl;
+        }
+        else if(totalAnnual>500000 && totalAnnual<1000000){
+          tax=20;
+          cout<<tax<<endl;
+        }
+        else{
+          tax=30;
+          cout<<tax<<endl;
+        }
+    if(annualTaxSaving>=150000)
+    {
         totalAnnual=totalAnnual-400000;
-        cout<<totalAnnual-tax<<endl;
+        tax=(totalAnnual*tax)/100;
+        cout<<tax<<endl;
+        cout<<totalAnnual+400000-tax<<endl;
     }
     else{
+        tax=(totalAnnual*tax)/100;
+        cout<<tax<<endl;
         cout<<totalAnnual-tax<<endl;
     }
     
